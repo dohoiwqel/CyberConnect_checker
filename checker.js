f5710b77671a00798a77f87fd87325b7264041d0
dohoiwqel <98800441+dohoiwqel@users.noreply.github.com>
2023-05-06 15:48:21 +0300

    Update checker.js

--- checker.js
+++ checker.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 
 async function main() {
     const wallets = fs.readFileSync('wallets.txt', 'utf-8').split('\r\n')
-    const winners = fs.readFileSync('winners.txt', 'utf-8').split('\n')
+    const winners = fs.readFileSync('winners.txt', 'utf-8').split('\r\n')
     
     for(let wallet of wallets) {
         if(winners.includes(wallet)) {
