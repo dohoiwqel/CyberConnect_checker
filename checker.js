96ab6022b42f2d0bc0bead55f4d5c23474356671
dohoiwqel <98800441+dohoiwqel@users.noreply.github.com>
2023-05-06 15:42:51 +0300

    Add files via upload

---
+++ checker.js
@@ -0,0 +1,15 @@
+const fs = require('fs')
+
+async function main() {
+    const wallets = fs.readFileSync('wallets.txt', 'utf-8').split('\r\n')
+    const winners = fs.readFileSync('winners.txt', 'utf-8').split('\n')
+    
+    for(let wallet of wallets) {
+        if(winners.includes(wallet)) {
+            console.log(wallet)
+        }
+    }
+    
+}
+
+main()
